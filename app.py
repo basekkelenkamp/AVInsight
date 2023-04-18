800ab9d7b4d671a5f7782391cc2db347ba5b722a
Bas Ekkelenkamp <basekkelenkamp1@gmail.com>
2023-04-18 17:00:09 +0200

    add styling & ram

--- app.py
+++ app.py
@@ -1,29 +1,68 @@
+import gpustat
 from flask import Flask, render_template
 from flask_socketio import SocketIO
 import psutil
 import time
 import threading
+import subprocess
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "secretkey"
 socket_io = SocketIO(app)
 
 
-def get_cpu_usage():
-    """Function to get CPU usage"""
+def get_metrics():
     while True:
-        cpu_percent = psutil.cpu_percent(interval=1, percpu=False)
+        cpu_percent = psutil.cpu_percent(interval=0.1, percpu=False)
+        # gpu_percent = get_gpu_usage('mac')
+
+        virtual_memory = psutil.virtual_memory()
+        ram_percent = virtual_memory.percent
+        total_memory = virtual_memory.total
+        used_memory = virtual_memory.used
+
+        # available_memory = virtual_memory.available
+        # free_memory = virtual_memory.free
 
         # Emit CPU usage to the connected clients
         socket_io.emit(
-            "cpu_usage",
+            "cpu-usage-chart",
             {
                 "data": [cpu_percent],
                 "labels": [],
             },
-            namespace="/test",
+            namespace="/metrics",
         )
-        time.sleep(1)
+        socket_io.emit(
+            "ram-usage-chart",
+            {
+                "data": [ram_percent],
+                "labels": [],
+            },
+            namespace="/metrics",
+        )
+        time.sleep(0.1)
+
+
+def get_gpu_usage(gpu_type="nvidia"):
+    if gpu_type == "nvidia":
+        try:
+            gpu_stats = gpustat.new_query()
+            return gpu_stats.gpus[0].utilization
+        except Exception as e:
+            print(f"Failed to get GPU usage: {e}")
+            return 0.0
+    else:
+        try:
+            output = subprocess.check_output(["intel_gpu_top", "-J"])
+            return float(
+                output.decode("utf-8").strip().split("\n")[1].split(":")[1].strip()
+            )
+
+        except subprocess.CalledProcessError:
+            # Handle any errors that may occur during command execution
+            print("Error: Failed to get GPU usage.")
+            return None
 
 
 @app.route("/")
@@ -31,17 +70,17 @@ def index():
     return render_template("index.html")
 
 
-@socket_io.on("connect", namespace="/test")
+@socket_io.on("connect", namespace="/metrics")
 def connect():
     """Event handler for socket.io connection"""
     print("Client connected")
     # Start a separate thread to get CPU usage in real-time
-    thread = threading.Thread(target=get_cpu_usage)
+    thread = threading.Thread(target=get_metrics)
     thread.daemon = True
     thread.start()
 
 
-@socket_io.on("disconnect", namespace="/test")
+@socket_io.on("disconnect", namespace="/metrics")
 def disconnect():
     """Event handler for socket.io disconnection"""
     print("Client disconnected")
--- static/js/draw_charts.js
+++ static/js/draw_charts.js
@@ -1,4 +1,4 @@
-var socket = io.connect('http://' + document.domain + ':' + location.port + '/test');
+var socket = io.connect('http://' + document.domain + ':' + location.port + '/metrics');
 
 var data = {
     labels: [],
@@ -21,19 +21,62 @@ var options = {
     }
 }
 
-var chart = new Chartist.Line('#cpu-usage-chart', data, options);
+function init() {
+    let charts = document.getElementsByClassName("ct-chart");
 
-socket.on('cpu_usage', function (data) {
-    var labels = data.labels;
-    var new_data_point = data.data;
+    for (let c of charts) {
+        let chart = new Chartist.Line('#' + c.id, data, options);
 
-    chart.update({
-        labels: labels,
-        series: [chart.data.series[0].concat(new_data_point)]
-    });
+        socket.on(c.id, function (data) {
+            var labels = data.labels;
+            var new_data_point = data.data;
+        
+            chart.update({
+                labels: labels,
+                series: [chart.data.series[0].concat(new_data_point)]
+            });
+        
+            if (chart.data.series[0].length > 30) {
+                chart.data.labels.splice(0, 1);
+                chart.data.series[0].splice(0, 1);
+            }
+        });
+        
 
-    if (chart.data.series[0].length > 30) {
-        chart.data.labels.splice(0, 1);
-        chart.data.series[0].splice(0, 1);
     }
-});
+}
+init()
+
+
+// var cpu_chart = new Chartist.Line('#cpu-usage-chart', data, options);
+// var ram_chart = new Chartist.Line('#ram-usage-chart', data, options);
+
+// socket.on('cpu_usage', function (data) {
+//     var labels = data.labels;
+//     var new_data_point = data.data;
+
+//     chart.update({
+//         labels: labels,
+//         series: [chart.data.series[0].concat(new_data_point)]
+//     });
+
+//     if (chart.data.series[0].length > 30) {
+//         chart.data.labels.splice(0, 1);
+//         chart.data.series[0].splice(0, 1);
+//     }
+// });
+
+// socket.on('ram_usage', function (data) {
+//     var labels = data.labels;
+//     var new_data_point = data.data;
+
+//     chart.update({
+//         labels: labels,
+//         series: [chart.data.series[0].concat(new_data_point)]
+//     });
+
+//     if (chart.data.series[0].length > 30) {
+//         chart.data.labels.splice(0, 1);
+//         chart.data.series[0].splice(0, 1);
+//     }
+// });
